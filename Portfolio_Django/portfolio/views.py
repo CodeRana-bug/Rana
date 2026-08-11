3861816db869e73cc861073caa13b7801af0b5b5
CodeRana-bug <rahulwebdigitalit@gmail.com>
2026-08-11 11:47:51 +0530

    Done

--- Portfolio_Django/portfolio/views.py
+++ Portfolio_Django/portfolio/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import ContactMessage
 
 
 def home(request):
+
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+
+        ContactMessage.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message
+        )
+
+        return redirect("/#contact")
+
     return render(request, "portfolio/index.html")
