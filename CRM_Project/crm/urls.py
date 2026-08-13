ee496ffd7e0af34ab39ce8ce7724848d1989e4e6
CodeRana-bug <rahulwebdigitalit@gmail.com>
2026-08-13 17:56:26 +0530

    done

--- CRM_Project/crm/urls.py
+++ CRM_Project/crm/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from . import views
 from .views import login_view, logout_view
+from django.urls import path
 from django.contrib.auth import views as auth_views
+
+from . import views
+from .views import login_view
 
 urlpatterns = [
 
@@ -107,36 +109,40 @@ urlpatterns = [
         logout_view,
         name="logout"
     ),
+    # ================= PASSWORD RESET =================
+
     path(
         "password-reset/",
-            auth_views.PasswordResetView.as_view(
-                template_name="crm/password_reset.html"
-            ),
+        auth_views.PasswordResetView.as_view(
+            template_name="crm/password_reset.html",
+            email_template_name="crm/password_reset_email.html",
+            success_url="/password-reset/done/"
+        ),
         name="password_reset"
     ),
 
     path(
         "password-reset/done/",
-            auth_views.PasswordResetDoneView.as_view(
-                template_name="crm/password_reset_done.html"
-            ),
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="crm/password_reset_done.html"
+        ),
         name="password_reset_done"
     ),
 
     path(
         "password-reset-confirm/<uidb64>/<token>/",
-            auth_views.PasswordResetConfirmView.as_view(
-                template_name="crm/password_reset_confirm.html"
-            ),
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="crm/password_reset_confirm.html",
+            success_url="/password-reset/complete/"
+        ),
         name="password_reset_confirm"
     ),
 
     path(
-        "password-reset-complete/",
-            auth_views.PasswordResetCompleteView.as_view(
-                template_name="crm/password_reset_complete.html"
-            ),
+        "password-reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="crm/password_reset_complete.html"
+        ),
         name="password_reset_complete"
     ),
-
 ]
